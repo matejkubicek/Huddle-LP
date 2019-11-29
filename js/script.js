ab2b13a128e8b6ddaf52c4787645b543b70f3110
Matěj Kubíček <matej.kubicek@gmail.com>
2019-11-29 19:02:07 +0100

    JS - change .nav background after scroll

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 $(function () {
+    // Smooth scrolling
     $('a[href^="#"]').click(function () {
-        let target = $(this).attr("href");
+        let target = $(this).attr('href');
         $('html, body').animate({
             scrollTop: $(target).offset().top
         }, 'slow');
         return false;
     });
+
+    // Change navbar background after scroll
+
+    $(window).scroll(function () {
+        let scroll = $(window).scrollTop();
+        if (scroll > 30) {
+            $('.nav').css({'background': 'rgba(235, 251, 255, 0.97)', 'box-shadow': '0 0 8px rgba(0, 0, 0, 0.15)'});
+        } else {
+            $('.nav').css({'background': 'none', 'box-shadow': 'none'});
+        }
+    })
+
 });
